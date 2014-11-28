04ca1ef3934c13fb91b2ff64f31ea21cc0802427
alarco-l <saw.darky@gmail.com>
2014-11-28 19:17:36 +0100

    Add graphic

--- MediaPlayerv2/MediaPlayerv2/MainWindow.xaml.cs
+++ MediaPlayerv2/MediaPlayerv2/MainWindow.xaml.cs
@@ -27,17 +27,17 @@ namespace MediaPlayerv2
 
     public class ViewModelBase : INotifyPropertyChanged
     {
-        private bool            _canExecute;
-        private MediaElement    _media;
-        private bool            isPaused = false;
-        private Image           _playImage = new Image();
+        private bool _canExecute;
+        private MediaElement _media;
+        private bool isPaused = false;
+        private Image _playImage = new Image();
 
         public Image playImage
         {
             get { return _playImage; }
-            set 
-            { 
-                _playImage.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_play.png");
+            set
+            {
+                _playImage.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/play.png");
                 RaisePropertyChanged("toto");
             }
         }
@@ -56,7 +56,7 @@ namespace MediaPlayerv2
         {
             _canExecute = true;
             _media = media;
-            _playImage.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_play.png");
+           _playImage.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/play.png");
         }
         private ICommand _clickCommand;
         public ICommand ClickCommand
@@ -74,31 +74,35 @@ namespace MediaPlayerv2
                 return _stopCommand ?? (_stopCommand = new CommandHandler(() => stopAction(), _canExecute));
             }
         }
-        
+
         public void stopAction()
         {
             _media.Stop();
         }
-        
+
         public void MyAction()
         {
+            BitmapImage bplay = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/play.png");
+            BitmapImage bpause = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/pause.png");
+            ImageBrush brush = new ImageBrush();
+
             if (!isPaused)
             {
-                _playImage.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_pause.png");
-                //((MainWindow)System.Windows.Application.Current.MainWindow).Play.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_pause.png");
+                brush.ImageSource = bpause;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).Play.Background = brush;
                 if (MainWindow.getFileName() != null)
                 {
                     _media.Source = new Uri(MainWindow.getFileName(), UriKind.RelativeOrAbsolute);
                     //isOk = true;
                 }
                 _media.Play();
-                ((MainWindow)System.Windows.Application.Current.MainWindow).Text.Text = MainWindow.getFileName();
+               // ((MainWindow)System.Windows.Application.Current.MainWindow).Text.Text = MainWindow.getFileName();
                 isPaused = true;
             }
             else
             {
-                _playImage.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_play.png");
-                //((MainWindow)System.Windows.Application.Current.MainWindow).Play.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_play.png");
+                brush.ImageSource = bplay;
+                ((MainWindow)System.Windows.Application.Current.MainWindow).Play.Background = brush;
                 _media.Pause();
                 isPaused = false;
             }
@@ -139,17 +143,17 @@ namespace MediaPlayerv2
 
     public partial class MainWindow : Window
     {
-        DispatcherTimer         timer;   
-        public delegate void    timerTick();   
-        timerTick               tick;
-        private bool            isPlaying = false;
-        private bool            isPaused;
-        FolderBrowserDialog     folder = new System.Windows.Forms.FolderBrowserDialog();
-        OpenFileDialog          dialogBox = new System.Windows.Forms.OpenFileDialog();
-        private static string   fileName;
-        private bool            isOk = false;
-        private bool            isFullScreen = false;
-  
+        DispatcherTimer timer;
+        public delegate void timerTick();
+        timerTick tick;
+        private bool isPlaying = false;
+        private bool isPaused;
+        FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+        OpenFileDialog dialogBox = new System.Windows.Forms.OpenFileDialog();
+        private static string fileName;
+        private bool isOk = false;
+        private bool isFullScreen = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -165,7 +169,7 @@ namespace MediaPlayerv2
             return fileName;
         }
 
-        void timer_Tick(object sender, EventArgs e)   
+        void timer_Tick(object sender, EventArgs e)
         {
             Dispatcher.Invoke(tick);
         }
@@ -180,19 +184,21 @@ namespace MediaPlayerv2
             return transition;
         }
 
-         void changeStatus()
-        {   
-            if (isPlaying)   
-                TimeLine.Value = MediaElement.Position.TotalMilliseconds;
-            if (MediaElement.Volume == 0)
-                Volume.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/mute.png");
-            else
-                Volume.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_mute.png");
+        void changeStatus()
+        {
+            if (isPlaying)
+			{
+                TimeLine.Value = MediaElement.Position.TotalSeconds;
+ 			}
+				//if (MediaElement.Volume == 0)
+            //    Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/mute.png");
+            //else
+            //    Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/button_mute.png");
         }
 
         private void Element_MediaOpened(object sender, EventArgs e)
         {
-            TimeLine.Maximum = MediaElement.NaturalDuration.TimeSpan.TotalMilliseconds;
+            TimeLine.Maximum = MediaElement.NaturalDuration.TimeSpan.TotalSeconds;
         }
 
         private void Element_MediaEnded(object sender, EventArgs e)
@@ -206,7 +212,7 @@ namespace MediaPlayerv2
             dialogBox.InitialDirectory = folder.SelectedPath;
             dialogBox.FileName = null;
 
-            DialogResult result =  dialogBox.ShowDialog();
+            DialogResult result = dialogBox.ShowDialog();
 
             if (result == System.Windows.Forms.DialogResult.OK)
                 fileName = dialogBox.FileName;
@@ -219,8 +225,8 @@ namespace MediaPlayerv2
 
             file = (String[])e.Data.GetData(System.Windows.Forms.DataFormats.FileDrop, true);
             MediaElement.Source = new Uri(file[0]);
-            Text.Text = file[0];
-            Play.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_pause.png");
+      //      Text.Text = file[0];
+        //    Play.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/button_pause.png");
             isPlaying = true;
             isPaused = false;
             MediaElement.Play();
@@ -245,22 +251,29 @@ namespace MediaPlayerv2
         private void playVideo(object sender, RoutedEventArgs e)
         {
             timer.Start();
+            BitmapImage bplay = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/play.png");
+            BitmapImage bpause = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/pause.png");
+            ImageBrush brush = new ImageBrush();
+
             if (!isPaused)
             {
-                Play.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_pause.png");
+                brush.ImageSource = bpause;
+                Play.Background = brush;
+                   
                 if (fileName != null && !isOk)
                 {
                     MediaElement.Source = new Uri(fileName, UriKind.RelativeOrAbsolute);
                     isOk = true;
                 }
                 MediaElement.Play();
-                Text.Text = fileName;
+               // Text.Text = fileName;
                 isPaused = true;
                 isPlaying = true;
             }
             else
             {
-                Play.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_play.png");
+                brush.ImageSource = bplay;
+                Play.Background = brush;
                 MediaElement.Pause();
                 isPlaying = false;
                 isPaused = false;
@@ -277,15 +290,15 @@ namespace MediaPlayerv2
         {
             if (MediaElement.IsMuted == true)
             {
-                Volume.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/button_mute.png");
+               // Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/button_mute.png");
                 MediaElement.IsMuted = false;
-                VolumeLine.Value = 0.5;
+              //  VolumeLine.Value = 0.5;
             }
             else
             {
-                Volume.Source = getNewImage("C:/Users/dasson_w/Desktop/Ressource PointNet/mute.png");
+               // Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/mute.png");
                 MediaElement.IsMuted = true;
-                VolumeLine.Value = 0;
+                //VolumeLine.Value = 0;
             }
         }
 
@@ -303,5 +316,76 @@ namespace MediaPlayerv2
             MediaElement.Play();
             timer.Start();
         }
+
+        private void Play_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			timer.Start();
+            BitmapImage bplay = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/play.png");
+            BitmapImage bpause = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/pause.png");
+            ImageBrush brush = new ImageBrush();
+
+            if (!isPaused)
+            {
+                brush.ImageSource = bpause;
+                Play.Background = brush;
+                Play.OpacityMask = brush;
+                if (fileName != null && !isOk)
+                {
+                    MediaElement.Source = new Uri(fileName, UriKind.RelativeOrAbsolute);
+                    isOk = true;
+                }
+                MediaElement.Play();
+               // Text.Text = fileName;
+                isPaused = true;
+                isPlaying = true;
+            }
+            else
+            {
+                brush.ImageSource = bplay;
+                Play.OpacityMask = brush;
+                Play.Background = brush;
+                MediaElement.Pause();
+                isPlaying = false;
+                isPaused = false;
+            }
+        }
+
+        private void sound_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            BitmapImage bsound = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/mute.png");
+            BitmapImage bmute = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/sound.png");
+            ImageBrush brush = new ImageBrush();
+            if (MediaElement.IsMuted == true)
+            {
+                brush.ImageSource = bmute;
+				sound.Background = brush;
+				sound.OpacityMask= brush;
+                // Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/button_mute.png");
+                MediaElement.IsMuted = false;
+                VolumeLine.Value = 0.5;
+            }
+            else
+            {
+                brush.ImageSource = bsound;
+                sound.Background = brush;
+				sound.OpacityMask = brush;
+                // Volume.Source = getNewImage("C:/Users/ludovic/Desktop/MediaPlayerv2/Ressource PointNet/mute.png");
+                MediaElement.IsMuted = true;
+                VolumeLine.Value = 0;
+            }
+        }
+
+        private void TimeLine_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (MediaElement.Source != null)
+            {
+                TimeLine.Value = e.GetPosition(TimeLine).X / TimeLine.ActualWidth * TimeLine.Maximum;
+                double time = MediaElement.NaturalDuration.TimeSpan.Ticks * (TimeLine.Value / TimeLine.Maximum);
+                Console.Write(time);
+                Console.Write("\n");
+                MediaElement.Position = new TimeSpan(0,0,(int)time,0);
+            }
+        }
+
     }
 }
